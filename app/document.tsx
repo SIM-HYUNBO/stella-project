59d2ed7d5e58a365f3f90399ac0bd96b1b24d5e3
심현보 <hbsim0605@gmail.com>
2025-11-30 21:14:26 +0900

    사파리 기능 추가

--- app/document.tsx
+++ app/document.tsx
@@ -7,7 +7,15 @@ export default function Document() {
         {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#f97316" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+
+        {/* iOS 홈 화면 아이콘 */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-icon-167.png" />
+
+        {/* iOS Safari에서 전체 화면으로 실행 */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body>
         <Main />
