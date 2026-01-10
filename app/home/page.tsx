cbb2cea8fb4441ca66fe8475431067c78a622d47
SIM-HYUNBO <hbsim0605@gmail.com>
2026-01-10 19:04:02 +0900

    글씨 크기 조정

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
       {/* 오른쪽 버튼 영역 */}
       <div className="fixed top-5 right-20 z-50 flex items-center gap-3 mt-2">
         <Link href="/pro">
-          <span className="px-6 py-3 bg-red-50 text-red-900 rounded-3xl border border-red-400">
+          <span className="px-4 py-2 text-sm bg-red-50 text-red-900 rounded-3xl border border-red-400">
             Pro 구입
           </span>
         </Link>
@@ -89,7 +89,7 @@ export default function Home() {
 
         {!user && (
           <Link href="/login">
-            <span className="px-6 py-3 bg-yellow-50 text-orange-900 rounded-3xl border border-yellow-400">
+            <span className="px-4 py-2 text-sm bg-yellow-50 text-orange-900 rounded-3xl border border-yellow-400">
               로그인
             </span>
           </Link>
