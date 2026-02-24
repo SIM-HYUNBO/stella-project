d9372a6f687bb071071384546ca50a11938bfce6
SIM-HYUNBO <hbsim0605@gmail.com>
2026-02-24 20:07:22 +0900

    에러 고침 완.

--- app/board/page.js
+++ app/board/page.js
@@ -25,28 +25,11 @@ export default function BoardPage() {
       {/* 본문 */}
       <div className="flex-1 bg-white dark:bg-gray-800 rounded-xl shadow p-6">
         <p className="text-lg text-gray-700 dark:text-gray-200">
-       천재들의 스터디 그룹!!
+          특별 이벤트 없음
          <br />
-         기간:2026년 2월 22일~2027년 2월 22일
-         <br />
-        천재들의 그룹!스터디 그룹!
-         <br />
-         1년동안 마음에 드는 친구들과 모여 스터디 그룹을 만들어 보세요!
-         <br />
-         모여서 실시간 채팅/통화도 하고,
-         <br />
-         토의/토론하며 천재 되기!
-        <br />
-         지금 바로 스터디 그룹 만들어 볼까요?
-        <br /> 
-          -홈페이지에 대해 불편한 시설이 있다면 댓글로 남겨주세요!
+          홈페이지에 대해 불편한 시설이 있다면 댓글로 남겨주세요!
         </p>
-        <button
-          onClick={() => router.push("/event")}
-          className="px-5 py-2.5 mt-10 bg-orange-400 text-white rounded-lg transition-all"
-        >
-          스터디 그룹 만들기
-        </button>
+        
       </div>
     </div>
   );
--- app/event/page.tsx
+++
@@ -1,7 +0,0 @@
-// pages/event.tsx 또는 app/event/page.tsx
-"use client"; // 꼭 최상단
-import App from "@/components/SGC";
-
-export default function EventPage() {
-  return <App />;
-}
