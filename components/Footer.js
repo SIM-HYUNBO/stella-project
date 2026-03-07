c0ce1a3f851d06d05c45eae85b4c3f69780334c4
SIM-HYUNBO <hbsim0605@gmail.com>
2026-03-08 00:42:48 +0900

    헤더-푸터 수정

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-orange-200 dark:bg-gray-400 w-full">
+    <footer className="bg-orange-200 w-full">
       <h2 className="h-20 pt-7 pl-10 mt-16 ml-10">©2025 WAGIE - by Stella. All rights reserved.          </h2>
     </footer>
   );
