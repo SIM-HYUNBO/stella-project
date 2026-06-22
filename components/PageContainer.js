1f892103d4fc3f72e1498265517984cc0364a455
SIM-HYUNBO <hbsim0605@gmail.com>
2026-06-23 00:42:56 +0900

    feat: 하단 푸터 네비에서 다이어리 제거 (v0.5.48)

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -52,17 +52,6 @@ const NAV_ITEMS = [
     ),
   },
   {
-    label: "다이어리",
-    path: "/diary",
-    icon: () => (
-    <svg width="22" height="22" viewBox="0 0 24 24" fill="none"
-        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-      </svg>
-    ),
-  },
-  {
     label: "플래너",
     path: "/planner",
     icon: () => (
